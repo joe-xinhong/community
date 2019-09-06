44031fe8772ced064c974dc390065ccf3dffa44f
陆新红 <xinhong8156@qq.com>
2019-09-06 15:08:01 +0800

    使用Cookie将token记录，查询用户放进Session

--- src/main/java/lift/miao/community/controller/AuthorizeController.java
+++ src/main/java/lift/miao/community/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 /**
@@ -35,7 +36,7 @@ public class AuthorizeController {
     
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
-                           HttpServletRequest request){
+                           HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -46,14 +47,17 @@ public class AuthorizeController {
         GitUser gitUser = githubProvider.getUser(accessToken);
         if(gitUser!=null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitUser.getName());
             user.setAccountId(String.valueOf(gitUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
             //登录成功，写cookie和Session
-            request.getSession().setAttribute("user",gitUser);
+            Cookie cookie = new Cookie("token",token);
+            response.addCookie(cookie);
+            //request.getSession().setAttribute("user",gitUser);
             //重定向到首页
             return "redirect:/";
         }else {
--- src/main/java/lift/miao/community/mapper/UserMapper.java
+++ src/main/java/lift/miao/community/mapper/UserMapper.java
@@ -3,6 +3,9 @@ package lift.miao.community.mapper;
 import lift.miao.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 /**
 * @Description:    user的Mapper
 * @Author:         Joe
@@ -13,4 +16,7 @@ public interface UserMapper {
 
     @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
+
+    @Select("select * from user where token=#{token}")
+    User findByToken(@Param("token") String token);
 }
