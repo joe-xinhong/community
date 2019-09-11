2c04cf42a73dc3dfd05ae40949b4f6c18dd9eb5b
陆新红 <xinhong8156@qq.com>
2019-09-11 16:02:59 +0800

    修改静态资源被拦截问题，两种方式均已说明

--- src/main/java/lift/miao/community/interceptor/WebConfig.java
+++ src/main/java/lift/miao/community/interceptor/WebConfig.java
@@ -12,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 * @CreateDate:     2019/9/10 21:26
 */
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -20,13 +20,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-
         registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
     }
 
-    @Override
+    /**
+     * 静态资源放行(释放此段代码或者注掉@@EnableWebMvc均可达到效果)
+     * @param registry
+     */
+    /*@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
 
-    }
+    }*/
 }
